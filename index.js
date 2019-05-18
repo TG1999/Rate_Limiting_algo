848e979178541535729fcc6f9c5c2cf8e585bb56
TG1999 <tushar.goel.dav@gmail.com>
2019-05-18 22:31:05 +0530

    bucket token algo
    Signed-off-by: TG1999 <tushar.goel.dav@gmail.com>

---
+++ index.js
@@ -0,0 +1,54 @@
+class Bucket {
+
+    constructor(capacity, fillAfterSeconds, fillQuantiy, initial=0) {
+        this.capacity = capacity;
+        if(!initial){
+            this.tokens=capacity;
+        }
+        this.initial=initial;
+        this.tokens = initial;
+        this.fillQuantiy=fillQuantiy
+        setInterval(() => this.addToken(), 1000*fillAfterSeconds);
+    }
+
+    addToken() {
+        if (this.tokens < this.capacity) {
+            this.tokens += this.fillQuantiy;
+        }
+    }
+
+    take() {
+        if (this.tokens > 0) {
+            this.tokens -= 1;
+            return true;
+        }
+
+        return false;
+    }
+}
+const bucket=new Bucket(10,2,5)
+const express = require('express');
+const app = express();
+
+function limitRequests(perSecond, maxBurst) {
+    const bucket = new TokenBucket(maxBurst, perSecond);
+
+    // Return an Express middleware function
+    return function limitRequestsMiddleware(req, res, next) {
+        if (bucket.take()) {
+            next();
+        } else {
+            res.status(429).send('Rate limit exceeded');
+        }
+    }
+}
+
+
+app.get('/',
+    limitRequests(5, 10), // Apply rate limiting middleware
+    (req, res) => {
+        res.send('Hello from the rate limited API');
+    }
+);
+
+app.listen(3000, () => console.log('Server is running'));
